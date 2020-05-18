ee31fef056e15de9b7bf304bb0a4b9e4b8fed97a
e-victoria <evictoria@protonmail.com>
2020-05-18 10:16:17 +0200

    Init project with dependencies and database connection

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # videoPlayer-back
+
+Run `npm install` to install all the required dependencies
+Run `node app.js` to run node server 
---
+++ controllers/dbConnection.js
@@ -0,0 +1,35 @@
+const pg = require('pg');
+
+const pool = new pg.Pool({
+    user: 'unixctvafkardi',
+    host: 'ec2-54-75-246-118.eu-west-1.compute.amazonaws.com\n',
+    database: 'd11qcci7olcrf8',
+    password: 'dece8a47e30aff10128c8e1da6b734865334b9155e2c533bb0122ca8a57fe984',
+    port: 5432,
+    ssl: { rejectUnauthorized: false }
+})
+
+async function getFromDb(query, callback) {
+    await pool.query(query, (err, res) => {
+        try {
+            callback(res.rows);
+        } catch(err)  {
+            console.log(err);
+        }
+
+    })
+}
+
+async function saveToDB(query, callback) {
+    console.log(query)
+    pool.query(query, (error, results) => {
+        if (error) {
+            throw error
+        } else {
+            callback('ok');
+        }
+    })
+}
+
+
+module.exports = {getFromDb: getFromDb, saveToDB: saveToDB};
