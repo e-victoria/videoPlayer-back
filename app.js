2f76c52a20bb7c20f961606591c35c11b0f654a6
e-victoria <evictoria@protonmail.com>
2020-05-19 14:35:05 +0200

    Check login data

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ app.use(cors({origin: 'http://localhost:4200'}));
 
 let videoController = require('./controllers/video/videoController');
 let signUpController = require('./controllers/login/signUpController');
+let signInController = require('./controllers/login/signInController');
 
 videoController.getMoviesList(app);
 videoController.getMovieByTitleAndDescription(app);
 signUpController.registerUser(app);
+signInController.checkUser(app);
 
 app.listen(3000);
