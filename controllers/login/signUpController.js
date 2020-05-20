bca45554fd76b049ba23a214d17f3a7383288201
e-victoria <evictoria@protonmail.com>
2020-05-20 10:53:48 +0200

    Check if user already exists

--- controllers/login/signUpController.js
+++ controllers/login/signUpController.js
@@ -8,11 +8,21 @@ registerUser = (app) => {
     }));
 
     app.post('/signup/new-user', (req, res) => {
-        function sendResponse(response) {
+        const sendResponse = (response) => {
             res.send(JSON.stringify(response));
             res.status(200).end();
         }
-        dbController.saveToDB(`INSERT INTO users(email, password) VALUES(${req.body['email']}, '${req.body['password']}';`, sendResponse);
+
+        const checkEmail = (response) => {
+            if(!response[0]) {
+                dbController.saveToDB(`INSERT INTO users(email, password) VALUES('${req.body['email']}', '${req.body['password']}');`, sendResponse);
+            } else {
+                res.send(JSON.stringify('Email already exists'));
+                res.status(200).end();
+            }
+        }
+
+        dbController.getFromDb(`SELECT email FROM users WHERE email = '${req.body['email']}';`, checkEmail);
     })
 }
 
